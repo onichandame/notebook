df866a9054062a4a2866aeb2d6d31e780adfd9c5
onichandame <zxinmyth@gmail.com>
2022-05-24 08:22:13 +0000

    fix: fix wss url

--- frontend/src/util/getSvcUrl.ts
+++ frontend/src/util/getSvcUrl.ts
@@ -1,6 +1,5 @@
 export const getSvcUrl = () =>
   import.meta.env.DEV
     ? import.meta.env.VITE_SERVER_URL
-    : window.location.protocol.startsWith(`https`)
-    ? `wss://`
-    : `ws://` + window.location.host + `/graphql`;
+    : (window.location.protocol.startsWith(`https`) ? `wss://` : `ws://`) +
+      window.location.host + `/graphql`;
