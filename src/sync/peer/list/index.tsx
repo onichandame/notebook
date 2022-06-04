96418d390fc2612c1d271f830d6d1244a8582553
onichandame <zxinmyth@gmail.com>
2022-06-04 07:41:22 +0000

    chore: fix peer creation entrypoint

--- src/sync/peer/list/index.tsx
+++ src/sync/peer/list/index.tsx
@@ -1,13 +1,16 @@
+import { Add } from '@mui/icons-material'
 import {
   Card,
   CardActionArea,
   CardContent,
   CardMedia,
   Grid,
+  SpeedDialAction,
   Typography,
 } from '@mui/material'
 import { FC, useEffect, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import { Actions } from '../../../actions'
 
 import { Loading, PlaceHolder } from '../../../common'
 import { useDb } from '../../../db'
@@ -26,51 +29,62 @@ export const List: FC = () => {
       .then(peers => setPeers(peers))
   }, [db])
   return sync && peers ? (
-    <Grid container direction="column" spacing={5}>
-      {peers.length ? (
-        <>
-          <Grid item>
-            <Grid
-              container
-              direction="row"
-              justifyContent="start"
-              alignItems="stretch"
-              spacing={3}
-              flexGrow={1}
-            >
-              {peers.map(peer => (
-                <Grid item key={peer.id} sx={{ display: `flex` }}>
-                  <Card sx={{ width: 180 }}>
-                    <CardActionArea onClick={() => navigate(peer.id)}>
-                      <CardMedia
-                        component="img"
-                        height="150"
-                        image={
-                          peer.icon ||
-                          'https://cdn2.iconfinder.com/data/icons/sync-and-data-exchange/64/PC-Sync-512.png'
-                        }
-                        alt={peer.name}
-                      />
-                      <CardContent>
-                        <Typography variant="h5" align="center">
-                          {peer.name}
-                        </Typography>
-                      </CardContent>
-                    </CardActionArea>
-                  </Card>
-                </Grid>
-              ))}
+    <>
+      <Grid container direction="column" spacing={5}>
+        {peers.length ? (
+          <>
+            <Grid item>
+              <Grid
+                container
+                direction="row"
+                justifyContent="start"
+                alignItems="stretch"
+                spacing={3}
+                flexGrow={1}
+              >
+                {peers.map(peer => (
+                  <Grid item key={peer.id} sx={{ display: `flex` }}>
+                    <Card sx={{ width: 180 }}>
+                      <CardActionArea onClick={() => navigate(peer.id)}>
+                        <CardMedia
+                          component="img"
+                          height="150"
+                          image={
+                            peer.icon ||
+                            'https://cdn2.iconfinder.com/data/icons/sync-and-data-exchange/64/PC-Sync-512.png'
+                          }
+                          alt={peer.name}
+                        />
+                        <CardContent>
+                          <Typography variant="h5" align="center">
+                            {peer.name}
+                          </Typography>
+                        </CardContent>
+                      </CardActionArea>
+                    </Card>
+                  </Grid>
+                ))}
+              </Grid>
             </Grid>
+          </>
+        ) : (
+          <Grid item>
+            <PlaceHolder>
+              <Link to="create">Create a peer</Link>
+            </PlaceHolder>
           </Grid>
-        </>
-      ) : (
-        <Grid item>
-          <PlaceHolder>
-            <Link to="create">Create a peer</Link>
-          </PlaceHolder>
-        </Grid>
-      )}
-    </Grid>
+        )}
+      </Grid>
+      <Actions>
+        <SpeedDialAction
+          icon={<Add />}
+          tooltipTitle="Create"
+          onClick={() => {
+            navigate(`create`)
+          }}
+        />
+      </Actions>
+    </>
   ) : (
     <Loading />
   )
